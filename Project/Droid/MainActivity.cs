6739c6b3f2fc6cbab50c6e464138f7117f44d5bd
Peter Beukema <pjdbeukema@gmail.com>
2015-09-26 11:43:39 +0200

    Project add
    Added new project from Tamarin to the repository

---
+++ Project/Droid/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿namespace dvign.Droid
+{
+	using Android.App;
+	using Android.Content.PM;
+	using Android.OS;
+
+	[Activity (Label = "d-vign.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+		}
+	}
+}
+
---
+++ Project/iOS/AppDelegate.cs
@@ -0,0 +1,15 @@
+﻿namespace dvign.iOS
+{
+	using Foundation;
+	using UIKit;
+
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
